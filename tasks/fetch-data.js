a4a2c986dd5be150321815513b26eaab79c8533a
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-01-21 23:02:12 -0600

    Correct file path

--- tasks/fetch-data.js
+++ tasks/fetch-data.js
@@ -2,7 +2,7 @@ const download = require('../src/download')
 const package = require('../package.json')
 const options = {
     url: package.config.data.remote.url,
-    dest: `./data/${package.config.data.remote.dest}`,
+    dest: package.config.data.remote.dest,
     force: false
 }
 download(options)
