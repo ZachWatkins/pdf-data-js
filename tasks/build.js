1525c7861ff5a6393af678fb7c811f01a9932dbd
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-01-23 22:13:37 -0600

    Begin working on a configuration object for the build process

--- tasks/build.js
+++ tasks/build.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-const options = {
-    src: undefined,
-    sheet: undefined,
-    dest: undefined,
-    select: [],
-    where: {}
-};
-const package = require('../package.json')
-const { WorkBookSheetToFile } = require('../src/workbook/write')
-const filters = Array.isArray(package.config.data.filtered)
-    ? package.config.data.filtered
-    : [package.config.data.filtered]
+const { WorkBookSheetToFile } = require('../src/write')
+const config = require('../package.json').config?.build || {}
+const arglist = ['dest', 'src', 'sheet', 'select', 'where', 'force', 'pretty']
+const options = arglist.reduce((values, arg) => {
+        const key = `npm_config_${arg}`
+        if (process.env.hasOwnProperty(key)) {
+            values[arg] = process.env[key]
+        } else if (config.hasOwnProperty(arg)) {
+            values[arg] = config[key]
+        }
+    }, {})
 for (let i = 0; i < filters.length; i++) {
     const filter = filters[i];
     WorkBookSheetToFile(filter)
