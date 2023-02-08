7aca344a84c10ecb7d8a597663edf184e44c172d
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-02-07 21:57:36 -0600

    Add support for element screenshots

--- tasks/screenshot.js
+++ tasks/screenshot.js
@@ -1,15 +1,36 @@
+/**
+ * Save a web page as an image.
+ * @author  Zachary K. Watkins
+ * @created 2023-02-06 9:46PM CST
+ * @package @zachwatkins/pdf-data-js
+ * @see https://pptr.dev/api/puppeteer.page.screenshot
+ */
 const puppeteer = require('puppeteer')
 const path = require('path')
 const url = 'file://' + path.resolve(`${__dirname}/../` + (process.env.npm_config_url || process.env.npm_package_config_url))
 const dest = path.resolve(`${__dirname}/../` + (process.env.npm_package_config_pdf_dest || 'build/index.png'))
-const screenshotOptions = {path: './build/index.png'}
 
-screenshot(url, screenshotOptions)
+screenshot({ url, ScreenshotOptions: { path: path.resolve(`${__dirname}/../build/index.png`)}})
 
-async function screenshot(url, options) {
+async function screenshot({ url, selectors, ScreenshotOptions }) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto(url)
-  await page.screenshot(options)
+  if (url) {
+    await page.goto(url)
+    await page.screenshot(ScreenshotOptions)
+  }
+  if (selectors) {
+    if (!Array.isArray(selectors)) {
+      selectors = [selectors]
+    }
+    for (let i = 0; i < selectors.length; i++) {
+      const selector = selectors[i]
+      await page.waitForSelector(selector)
+      const element = await page.$(selector)
+      await element.screenshot({
+        path: `build/element_${i+1}.png`,
+      })
+    }
+  }
   await browser.close()
 }
