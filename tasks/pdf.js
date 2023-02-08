13077eee6057762bece8fd304ff0fa45291b7c52
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-02-07 20:25:08 -0600

    Address cross-platform issue

--- tasks/pdf.js
+++ tasks/pdf.js
@@ -6,6 +6,10 @@
  * @package @zachwatkins/pdf-data-js
  */
 const puppeteer = require('puppeteer')
+const { platform } = require('os')
+const WINDOWS_PLATFORM = 'win32'
+const MAC_PLATFORM = 'darwin'
+const osPlatform = platform()
 const path = require('path')
 const package = require('../package.json')
 const url = process.env.npm_config_url || process.env.npm_package_config_url
@@ -20,6 +24,9 @@ urlToPDF(url, { ...package.config.pdf.options, path: dest })
  * @returns {Buffer} PDF file buffer.
  */
 async function urlToPDF(url, options) {
+    if (osPlatform === MAC_PLATFORM && 0 > url.indexOf('http://')) {
+      url = 'file://' + url
+    }
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     await page.goto(path.resolve(url), {waitUntil: 'networkidle0'})
