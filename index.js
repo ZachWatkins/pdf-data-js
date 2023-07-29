ade3e83003f3decb8feb4eb874b0f328e40a5571
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-29 11:10:12 -0500

    Remove root index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const download = require('./tasks/download')
-const build = require('./tasks/build')
-const pdf = require('./tasks/pdf')
-const screenshot = require('./tasks/screenshot')
-const open = require('./tasks/open')
