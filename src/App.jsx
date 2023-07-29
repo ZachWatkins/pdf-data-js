467560a28ad79074ec4b3b9a62307eb0c9e26eea
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-29 16:04:32 -0500

    Implement PDF renderer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { PDFViewer } from '@react-pdf/renderer';
+import MyDocument from './MyDocument'
 import dataSet from './dataSet.json'
 import Table from './components/Table'
 
@@ -8,6 +10,9 @@ export default function App() {
         <div>
             <h2>{dataSet.title}</h2>
             <Table data={dataSet.data} sortColumn='StudyYear' sortOrder='desc' />
+            <PDFViewer>
+                <MyDocument />
+            </PDFViewer>
         </div>
     )
 }
