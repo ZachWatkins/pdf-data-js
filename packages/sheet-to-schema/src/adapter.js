af8d8fa073adc65a3457929da77df78d2b564776
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-09 22:03:30 -0500

    Add file generation methods to default adapter

--- packages/sheet-to-schema/src/adapter.js
+++ packages/sheet-to-schema/src/adapter.js
@@ -77,6 +77,75 @@ export class Adapter {
       return acc
     }, [])
   }
+
+  /**
+   * Create the contents of a schema file from an object with cell column headers for keys and {[propType]: true} collections for values.
+   * @param {{[column: string]: {[propType: string]: boolean}}} types - Object with cell column headers for keys and {[propType]: true} collections for values.
+   * @param {object} options - File options.
+   * @returns {string} Schema file contents for an array of objects which conforms to the adapter's schema language.
+   */
+  getFileContents(types, options = {}) {
+    const { name = 'schema', withImport, withExport } = options
+    const lines = []
+
+    if (withImport) {
+      lines.push(this.fileHeader())
+    }
+
+    lines.push(`const ${name} = {`)
+
+    for (const prop in types) {
+      // Sorts types so null and optional are at the end of the array, and optional is at the very end of the array when present.
+      let types = Object.keys(types[prop]).sort((a, b) =>
+        'optional' === a
+          ? 1
+          : 'optional' === b
+          ? -1
+          : 'null' === a
+          ? 1
+          : 'null' === b
+          ? -1
+          : 0
+      )
+      let propKey = prop.indexOf(' ') > -1 ? `"${prop}"` : prop
+      lines.push(
+        `    ${propKey}: [` +
+          types
+            .reduce((acc, type) => {
+              acc.push(this.cellTypeSchema(type))
+              return acc
+            }, [])
+            .join(', ') +
+          '],\n'
+      )
+    }
+
+    lines.push('};')
+
+    if (withExport) {
+      lines.push(`export default ${name}`)
+    }
+
+    return lines.join('\n') + '\n'
+  }
+
+  /**
+   * Get the header text for a schema file.
+   * @param {object} options - Schema file header options.
+   * @returns {string} Schema file header.
+   */
+  fileHeader(options) {
+    return ''
+  }
+
+  /**
+   * Get the footer text for a schema file.
+   * @param {object} options - Schema file footer options.
+   * @returns {string} Schema file footer.
+   */
+  fileFooter(options) {
+    return 'export default schema'
+  }
 }
 
 export default { Adapter }
