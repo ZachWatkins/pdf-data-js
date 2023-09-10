fa053c9b74513d18c38866157ba52450b5266c4e
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-09 22:35:12 -0500

    Add initial Jest test for the schema factory

---
+++ packages/sheet-to-schema/src/schema-factory.spec.js
@@ -0,0 +1,8 @@
+import { SchemaFactory } from './schema-factory.js'
+
+describe('SchemaFactory', () => {
+  test('creates a schema from workbook data using a schema adapter', () => {
+    const factory = new SchemaFactory()
+    // Write your test case here
+  })
+})
